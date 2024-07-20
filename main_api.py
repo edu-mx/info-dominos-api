513cbee715ff964b5036c40e93ed86b2e25b12f6
edu-mx <diaseduardo139@gmail.com>
2024-07-20 16:49:19 -0300

    correction, access to frontend.

--- main_api.py
+++ main_api.py
@@ -1,14 +1,22 @@
 from typing import Union
 import whois
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
 
 def consultar_dominio(dominio):
     domain_info = whois.whois(dominio)
     return domain_info
 
+# Configuração de CORS para permitir todas as origens
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todas as origens
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/site/{dom}")
 def ler_dominio(dom: str, desc: Union[str, None] = None):
@@ -16,9 +24,9 @@ def ler_dominio(dom: str, desc: Union[str, None] = None):
     dic_info = {
         "descrição": desc,
         "domínio": result.domain_name,
-        "registrante": result.registrar,
+        "e-mails": result.emails,
         "data de criação": result.creation_date,
-        "data de espiração": result.expiration_date,
+        "data de expiração": result.expiration_date,
         "servidores de nome": result.name_servers,
         "status": result.status
     }
